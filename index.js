4395a2a7878a1aeffe87932c2584d05e699b26de
Ben Maxwell <ben.maxwell@bbc.co.uk>
2018-12-06 09:55:48 +0000

    Make actually work.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const compiler = require("c-preprocessor");
 
 module.exports = function(source) {
   const options = loaderUtils.getOptions(this);
+  const callback = this.async();
   compiler.compile(source, options, (err, result) => {
     this.callback(err, result);
   });
